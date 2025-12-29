bf2ceae2b9612a3d9fe1ddebed2f9bf4805c9895
Rachel Wood <rachelnwood98@gmail.com>
2025-12-29 10:36:43 -0700

    Completed day 6 part 1

---
+++ 2025/day_6/part_1.py
@@ -0,0 +1,41 @@
+def read_input(input_file_name):
+    result = []
+
+    with open(input_file_name) as file:
+        for line in file:
+            tokens = line.split()
+            if not tokens:
+                continue
+
+            # numeric line
+            if all(token.lstrip("-").isdigit() for token in tokens):
+                result.append([int(token) for token in tokens])
+            else:
+                # operator / symbol line
+                result.append(tokens)
+
+    return result
+
+def do_math(lines: list[list]) -> int:
+    row_count = len(lines)
+    overall_total = 0
+
+    for column_index in range(len(lines[0])):
+        is_adding = lines[row_count-1][column_index] == '+'
+        column_total = 0 if is_adding else 1
+
+        for row_index in range(row_count-1):
+            if is_adding:
+                column_total += lines[row_index][column_index]
+            else:
+                column_total *= lines[row_index][column_index]
+
+        overall_total += column_total
+
+    return overall_total
+
+# main program
+if __name__ == '__main__':
+    lines = read_input("puzzle_input")
+    total = do_math(lines)
+    print(total)
---
+++ 2025/day_6/test_part_1_cephalopod.py
@@ -0,0 +1,6 @@
+from day_6.part_1 import *
+
+def test_part_1_do_math():
+    lines = [[123, 328, 51, 64], [45, 64, 387, 23], [6, 98, 215, 314], ['*', '+', '*', '+']]
+    result = do_math(lines)
+    assert(result == 4277556)
